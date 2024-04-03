90f107f8701612824ffd1fbf84d7e8da9d4016d3
Annastenild <bcd934@alumni.ku.dk>
2024-04-03 11:48:32 +0200

    igen

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -1,6 +1,6 @@
 def keep_regs(df, regs):
     """ Example function. Keep only the subset regs of regions in data.
-
+    
     Args:
         df (pd.DataFrame): pandas dataframe 
 
@@ -14,3 +14,23 @@ def keep_regs(df, regs):
         df = df.loc[I == False] # keep everything else
     
     return df
+
+from types import SimpleNamespace
+
+import numpy as np
+
+class ASADClass:
+
+    def __init__(self):
+
+        par = self.par = SimpleNamespace() # parameters
+        sim = self.sim = SimpleNamespace() # simulation variables
+        datamoms = self.datamoms = SimpleNamespace() # moments in the data
+        moms = self.moms = SimpleNamespace() # moments in the model
+
+        # a. externally given parameters
+        par.alpha = None
+
+    def GDP(self):
+
+    return error 
