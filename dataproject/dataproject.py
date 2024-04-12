33f183160b3f66c8970d5c892f49ba65804986bf
Annastenild <bcd934@alumni.ku.dk>
2024-04-12 12:35:10 +0200

    anna bryger python file

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -36,3 +36,20 @@ class Solowclass:
     
     def print_head(self):
         print(self.data.head(5))
+
+
+import numpy as np
+
+class AlphaCalculator:
+    def __init__(self, data):
+        # Initialize the AlphaCalculator class with input data
+        self.data = data
+        
+    def calculate_alpha(self, Q_col, B_col, K_col, L_col):
+        # Calculate alpha value using the specified columns for Q, B, K, and L
+        ln_Q_t = np.log(self.data[Q_col])
+        ln_B_t = np.log(self.data[B_col])
+        ln_KL_t = np.log(self.data[K_col] * self.data[L_col])
+        
+        alpha_value = (ln_Q_t - ln_KL_t) / (ln_B_t - ln_KL_t)
+        return alpha_value
