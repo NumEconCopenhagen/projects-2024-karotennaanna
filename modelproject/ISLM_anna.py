56e647b3ad9e23d91ccb99ee6a3a2bf139eb1be3
Annastenild <bcd934@alumni.ku.dk>
2024-05-09 11:49:17 +0200

    anna tilføjelser

--- modelproject/ISLM_anna.py
+++ modelproject/ISLM_anna.py
@@ -1,49 +1,62 @@
 from scipy import optimize
 from types import SimpleNamespace
-
 import numpy as np
+import random
 
 class ISLMClass:
-
     def __init__(self):
+        self.par = SimpleNamespace()  # parameters
+        self.sim = SimpleNamespace()  # simulation variables
+        self.datamoms = SimpleNamespace()  # moments in the data
+        self.moms = SimpleNamespace()  # moments in the model
+
+    def ISLM_equations(self, variables, Y, C, I, G, r, T, P, M):
+        """
+        Args:
+            variables (list or tuple): Contains two variables to be solved for: Output and Interest Rate
+            Y (float): Output
+            C (float): Consumption
+            I (float): Investments
+            G (float): Government spending
+            r (float): Interest rate
+            T (float): Taxes
+            P (float): Consumer price index
+            M (float): Money supply
         
-        par = self.par = SimpleNamespace() # parameters
-        sim = self.sim = SimpleNamespace() # simulation variables
-        datamoms = self.datamoms = SimpleNamespace() # moments in the data
-        moms = self.moms = SimpleNamespace() # moments in the model
+        Returns:
+            ISLM for r_IS = r_LM
+        """
+        Y, r = variables  # Unpack variables assumed to be Output (Y) and Interest Rate (r)
+        r_IS = C + I + G - T  # Simplified IS equation: Y = C + I + G - T
+        r_LM = M / (P * Y) - r  # Simplified LM equation: M = P * Y * r
+        
+        return [Y - r_IS, r_LM]  # Return residuals for both equations
 
-    def ISLM_equations(variables, Y, C, I, G, r, T, P, M):
-    """
-    Args:
-        variables (list or tuple): Contains two variables to be solved for: physical capital and human capital
-        Y               (float): Output 
-        C               (float): Consumption 
-        I               (float): Investments 
-        G                 (float): Goverment spending
-        r                 (float): Interest rate
-        T             (float): Taxes
-        P             (float): Consumer price index 
-        M            (float): Money supply 
-    
-    Returns:
-        ISLM for r_IS=r_LM
-    """
-    # Variables to be solved for: Output and interest rate
-    Y, r = variables
-    
-    # Checks for edge cases, used in multi_start
-    if Y <= 0 or r <= 0:
-        # Return a very large residual to indicate a poor solution
-        return [np.inf, np.inf]
+    def multi_start(self, num_guesses=100, bounds=[(0, 1000), (0, 10)], fun=None):
+        """
+        Performs multi-start optimization to find the equilibrium solutions for Output and Interest Rate.
+        
+        Args:
+            num_guesses (int): The number of random initial guesses
+            bounds (list of tuple): The bounds for the random initial guesses for Y and r
+            fun (function): The function to be optimized, default uses ISLM_equations
+        
+        Returns:
+            Optimal Output and Interest Rate values
+        """
+        if fun is None:
+            fun = self.ISLM_equations
 
-    # Set Solow equations for k_{t+1}-k_{t} = 0 and h_{t+1}-h_{t} = 0 
-    r_IS=1/f*(e*Y-(M/P))
-    r_LM=1/d**(a+c+G-b*T)-(1-b)*Y)
+        smallest_residual = np.inf
+        best_solution = None
 
-    # Return equations
-    return r_IS, r_LM
+        # Loop through each random initial guess
+        for _ in range(num_guesses):
+            initial_guess = [np.random.uniform(low, high) for low, high in bounds]
+            sol = optimize.root(fun=fun, x0=initial_guess, args=(self.sim.Y, self.sim.C, self.sim.I, self.sim.G, self.sim.r, self.sim.T, self.sim.P, self.sim.M), method='hybr')
+            residual_norm = np.linalg.norm(sol.fun)
+            if residual_norm < smallest_residual:
+                smallest_residual = residual_norm
+                best_solution = sol.x
 
-
-
-    return result
-
+        return best_solution, smallest_residual
