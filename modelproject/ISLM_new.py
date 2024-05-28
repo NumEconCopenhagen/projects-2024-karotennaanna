cf9b58220423472af0c691218997eecec6c449d3
Annastenild <bcd934@alumni.ku.dk>
2024-05-28 22:31:38 +0200

    analysis ext

--- modelproject/ISLM_new.py
+++ modelproject/ISLM_new.py
@@ -150,3 +150,34 @@ class ISLMPlotter:
 
         self.model.M = new_M
         self.plot(Y_range, num_points, label_suffix=f'M={new_M}', include_initial_curves=True)
+    
+class ISLM_open:
+    def __init__(self, a, b, c, d, e, f, g, T, G, M, P, r, N, X, IM, e_rate, i_foreign):
+        # X is the export responsiveness to exchange rate, IM is the import function
+        self.par = SimpleNamespace(a=a, b=b, c=c, d=d, e=e, f=f, g=g, T=T, G=G, M=M, P=P, r=r, N=N, X=X, IM=IM, e_rate=e_rate, i_foreign=i_foreign)
+
+    def planned_expenditures(self, Y):
+        return self.private_consumption(Y) + self.investment_demand() + self.par.G + self.net_exports(Y)
+
+    def private_consumption(self, Y):
+        return self.par.a + self.par.b * (Y - self.par.T)
+
+    def investment_demand(self):
+        return self.par.c - self.par.d * self.par.r
+
+    def net_exports(self, Y):
+        # Net exports are a function of exchange rate and foreign demand
+        return self.par.X * self.par.e_rate - self.par.IM * Y
+
+    def real_demand_for_money(self, Y):
+        # Adjusted for foreign interest rates
+        return self.par.e * Y - self.par.f * (self.par.r - self.par.i_foreign)
+
+    def equilibrium_in_money_market(self, Y):
+        return self.par.M / self.par.P - self.real_demand_for_money(Y)
+
+    def IS_curve(self, Y):
+        return self.planned_expenditures(Y) - Y
+
+    def LM_curve(self, Y):
+        return (self.par.e / self.par.f) * Y - (1 / self.par.f) * (self.par.M / self.par.P)
